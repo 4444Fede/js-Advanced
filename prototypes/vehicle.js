f70c5abc053fbbabeb588b95d9b1d6a99c234dac
4444Fede <federico.vega@gm2dev.com>
2024-10-14 12:15:46 -0300

    feat(vehicle.js): add tests for Auto and Moto using inherited and custom methods

--- prototypes/vehicle.js
+++ prototypes/vehicle.js
@@ -22,7 +22,6 @@ Vehiculo.prototype.mostrarInfo = function() {
     return `Marca: ${this.marca}, Modelo: ${this.modelo}, Velocidad Maxima: ${this.velocidadMaxima}`;
 };
 
-
 Vehiculo.prototype.compararVelocidad = function(otroVehiculo){
     if (this.velocidadMaxima > otroVehiculo.velocidadMaxima){
         return `El ${this.marca} ${this.modelo} es mas rapido`
@@ -58,3 +57,30 @@ Moto.prototype.mostrarSidecar = function() {
         return `No tiene sidecar`
     }
 };
+
+const auto1 = new Auto('Toyota', 'Corolla', 180, 4);
+const auto2 = new Auto('Ford', 'Mustang', 250, 2);
+const moto1 = new Moto('Yamaha', 'MT-07', 200, false);
+const moto2 = new Moto('Harley-Davidson', 'Fat Boy', 160, true);
+
+console.log(auto1.mostrarInfo());
+console.log(auto2.mostrarInfo());
+console.log(moto1.mostrarInfo());
+console.log(moto2.mostrarInfo());
+
+console.log(auto1.abrirPuertas());
+console.log(auto2.abrirPuertas());
+console.log(moto1.mostrarSidecar());
+console.log(moto2.mostrarSidecar());
+
+console.log(auto1.acelerar(60)); 
+console.log(auto2.acelerar(120)); 
+console.log(auto2.acelerar(100)); 
+console.log(moto1.acelerar(100)); 
+console.log(moto2.acelerar(80)); 
+
+console.log(auto1.detener());
+console.log(moto1.detener());
+
+console.log(auto1.compararVelocidad(moto1));
+console.log(auto2.compararVelocidad(moto2));
