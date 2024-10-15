06c33b896936631fb62fe8c8be6486a6b6ab5796
4444Fede <federico.vega@gm2dev.com>
2024-10-15 12:39:11 -0300

    feat(raceConditions/script.js): initialize variables for ticket sales

---
+++ raceConditions/script.js
@@ -0,0 +1,7 @@
+let tickets = 10;
+let users = [
+  { name: "papa", ticketsRequested: 3 },
+  { name: "pepe", ticketsRequested: 5 },
+  { name: "pipi", ticketsRequested: 4 },
+  { name: "pupu", ticketsRequested: 2 },
+];
