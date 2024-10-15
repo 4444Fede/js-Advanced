1d49c09c99a15bb384331b9fbebdf07a3ffeb326
4444Fede <federico.vega@gm2dev.com>
2024-10-15 12:42:25 -0300

    feat(raceConditions/script.js): add function for users to get tickets

--- raceConditions/script.js
+++ raceConditions/script.js
@@ -5,3 +5,18 @@ let users = [
   { name: "pipi", ticketsRequested: 4 },
   { name: "pupu", ticketsRequested: 2 },
 ];
+
+function getTicket(user) {
+  return new Promise((resolve) => {
+    console.log(`${user.name} esta intentando comprar ${user.ticketsRequested} ticket(s).`);
+    setTimeout(() => {
+      if (tickets >= user.ticketsRequested) {
+        tickets -= user.ticketsRequested;
+        console.log(`${user.name} compro ${user.ticketsRequested} ticket(s). Quedan ${tickets} ticket(s)`);
+      } else {
+        console.log(`${user.name} no pudo comprar ${user.ticketsRequested} ticket(s). Insuficiente stock. Quedan ${tickets} ticket(s)`);
+      }
+      resolve();
+    }, Math.random() * 10000);
+  });
+}
